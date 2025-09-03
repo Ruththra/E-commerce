776075bfe8a95573b6f21e9754e27d04af4ba163
Ruththra <ruththu2003@gmail.com>
2025-09-03 23:59:58 +0530

    chore:sdcsdfc

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ import helmet from 'helmet';
 import morgan from'morgan';
 
 const app = express();
+const PORT = process.env.PORT ;
 
+app.use(express.json()); // Parse JSON request bodies
+app.use(cors());
 app.use(helmet())//security for http headers
 app.use(morgan('dev'))//log the requests
 
@@ -12,6 +15,6 @@ app.get('/test', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
