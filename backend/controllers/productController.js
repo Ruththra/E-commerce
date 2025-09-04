9406871a59e447ba8f7542d4b071a0de7f58a182
Ruththra <ruththu2003@gmail.com>
2025-09-04 23:58:37 +0530

    chore: set

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,5 @@
 export const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+
 };
 
 export const createProduct = async (req,res) => {
