d078da9b501e2a067f8fcafd14487bf448869eaa
Ruththra <ruththu2003@gmail.com>
2025-09-10 09:20:11 +0530

    chore: database credentials added and database initialized

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createProduct, getAllProducts } from '../controllers/productController';
+import { createProduct, getAllProducts } from '../controllers/productController.js';
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 import morgan from'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import {sql} from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
 
@@ -18,6 +19,25 @@ app.use(morgan('dev'))//log the requests
 
 app.use('/api/products',productRoutes);
 
-app.listen(PORT, () => {
-  console.log('Server is running on port '+PORT);
+async function initDB(){
+  try {
+    await sql`
+    CREATE TABLE IF NOT EXISTS products (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      image VARCHAR(255) NOT NULL,
+      price DECIMAL(10, 2) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    `
+  } catch (error) {
+    console.error('Error initializing database:', error);
+  }
+
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+  });
 });
