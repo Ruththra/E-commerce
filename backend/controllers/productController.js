1853ba0bc24065fece625e30a3b8679ccf8ea76d
Ruththra <ruththu2003@gmail.com>
2025-09-11 10:09:52 +0530

    chore: createPrdoduct function created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,36 @@
-export const getProducts = async (req, res) => {
+import { sql } from '../config/db.js';
 
+export const getProducts = async (req, res) => {
+    try{
+        // Fetch all products from the database
+        const products = await sql`
+        SELECT * FROM products 
+        ORDER BY created_at DESC
+        `;
+
+        console.log("Fetched products:", products);
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
+    }
 };
 
 export const getProduct = async (req, res) => {
-
+    try {
+        const { id } = req.params;
+        const products = await sql`
+            SELECT * FROM products WHERE id = ${id}
+        `;
+        if (products.length > 0) {
+            res.status(200).json(products[0]);
+        } else {
+            res.status(404).json({ error: 'Product not found' });
+        }
+    } catch (error) {
+        console.error('Error fetching product:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
 
 export const updateProduct = async (req, res) => {
@@ -15,5 +42,20 @@ export const deleteProduct = async (req, res) => {
 };
 
 export const createProduct = async (req,res) => {
+    const {name, image, price} = req.body;
+    try {
+        if(!name || !image || !price){
+            return res.status(400).json({success:false, message:'All fields are required'});
+        }
 
+        const newProduct = await sql`
+            INSERT INTO products (name, image, price) 
+            VALUES (${name}, ${image}, ${price})
+            RETURNING *
+        `;
+        res.status(201).json({success:true, message:'Product created successfully', data:newProduct[0]});
+    } catch (error) {
+        console.error('Error in createProduct function', error);
+        res.status(500).json({success:false, message:'Internal Server Error'});
+    }
 };
