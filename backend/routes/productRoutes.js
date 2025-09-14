805b8c125b815a179e4f26a10abe3e5cbcec85aa
Ruththra <ruththu2003@gmail.com>
2025-09-14 23:49:01 +0530

    empty

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,13 +9,8 @@ const router = express.Router();
 
 router.get('/', getProducts);
 router.get('/:id', getProduct);
-
 router.post('/',createProduct);
-
-
 router.put('/:id', updateProduct);
-
-
 router.delete('/:id', deleteProduct);
 
 export default router;
